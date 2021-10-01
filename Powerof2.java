8746bddb015479b79a3ff34de1b2ab1d60ca570a
kanchan-solanke <kanchanssolanke@gmail.com>
2021-10-01 07:52:45 +0530

    Find Power Of 2

---
+++ Powerof2.java
@@ -0,0 +1,18 @@
+class PowersOf2 {
+    public static void main(String[] args) {
+
+        // read in one command-line argument
+        int n = Integer.parseInt(args[0]);
+
+        int i = 0;                // count from 0 to N
+        int powerOfTwo = 1;       // the ith power of two
+
+        // repeat until i equals n
+        while (i <= n) {
+            System.out.println(i + " " + powerOfTwo);
+            powerOfTwo = 2 * powerOfTwo;                // double to get the next one
+            i = i + 1;
+        }
+
+    }
+}
